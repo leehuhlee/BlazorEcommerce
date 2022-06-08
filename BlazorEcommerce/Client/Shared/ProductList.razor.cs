f0efd8099361f3266e89231a1a135cea85baf97d
Hanna <hanna.lee@kehag.de>
2022-06-08 14:24:55 +0200

    Add project files.

---
+++ BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Shared
+{
+    public partial class ProductList
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        private static List<Product> Products = new List<Product>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await Http.GetFromJsonAsync<List<Product>>("api/product");
+            if (result != null)
+                Products = result;
+        }
+    }
+}
---
+++ BlazorEcommerce/Server/Program.cs
@@ -0,0 +1,43 @@
+global using Microsoft.EntityFrameworkCore;
+global using BlazorEcommerce.Shared;
+global using BlazorEcommerce.Server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+}); 
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwaggerUI();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseSwagger();
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
